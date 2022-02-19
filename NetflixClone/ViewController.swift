ab7e28fe9bc51598ad39d4ae2a29b24815db2dc7
Mohamed Abd-Elhamed Ahmed <73020992+Mohamedabdelhamed382@users.noreply.github.com>
2022-02-20 00:43:12 +0200

    Initial Commit

---
+++ NetflixClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixClone
+//
+//  Created by MOHAMED ABD ELHAMED AHMED on 20/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
